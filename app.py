997a822a353432e946a0c05f38530590ba5af3cc
pritom <pritom.rajkhowa@gmail.com>
2020-09-03 01:35:54 +0800

    seventh commit

--- app.py
+++ app.py
@@ -153,7 +153,14 @@ app.layout = html.Div([
           {'label':'gsv2008_true-unreach-call_true-termination-i.c', 'value': 'gsv2008_true-unreach-call_true-termination-i.c'},
           {'label':'hhk2008_true-unreach-call_true-termination-i.c', 'value': 'hhk2008_true-unreach-call_true-termination-i.c'},
           {'label':'jm2006_true-unreach-call_true-termination-i.c', 'value': 'jm2006_true-unreach-call_true-termination-i.c'},
-          {'label':'jm2006_variant_true-unreach-call_true-termination-i.c', 'value':'jm2006_variant_true-unreach-call_true-termination-i.c'}
+          {'label':'jm2006_variant_true-unreach-call_true-termination-i.c', 'value':'jm2006_variant_true-unreach-call_true-termination-i.c'},
+          {'label':'count_by_1_true-unreach-call_true-termination.c', 'value':'count_by_1_true-unreach-call_true-termination.c'},
+          {'label':'count_by_1_variant_true-unreach-call_true-termination.c', 'value':'count_by_1_variant_true-unreach-call_true-termination.c'},
+          {'label':'count_by_2_true-unreach-call_true-termination.c', 'value':'count_by_2_true-unreach-call_true-termination.c'},
+          {'label':'gauss_sum_true-unreach-call.i.p+cfa-reducer.c', 'value':'gauss_sum_true-unreach-call.i.p+cfa-reducer.c'},
+          {'label':'gauss_sum_true-unreach-call.i.v+cfa-reducer.c', 'value':'gauss_sum_true-unreach-call.i.v+cfa-reducer.c'},
+          {'label':'gauss_sum_true-unreach-call_true-termination.c', 'value':'gauss_sum_true-unreach-call_true-termination.c'}
+
     ], value='afnp2014_true-unreach-call-i.c'),
       html.H6(children='Input Program'),
       dcc.Textarea(
